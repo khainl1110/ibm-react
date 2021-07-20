99312d7789380d4d44bdc2c059533abd18e0f086
Khai Nguyen <khainl1110@gmail.com>
2021-07-19 21:29:22 -0700

    Adding some info

--- src/Components/AboutPage.js
+++ src/Components/AboutPage.js
@@ -11,6 +11,9 @@ export default function AboutPage() {
                 <li> Software engineer concepts such as low coupling, high cohesion for organizing code</li>
             </ul>
             <p>Github: @khainl1110</p>
+            <h1>AWS Serverless</h1>
+            <p>This is a new project with backend is complete on the cloud using AWS Serverless Suite</p>
+            <p>Serverless Architecture: API Gateway -> AWS Lambda -> DynamoDB</p>
         </div>
         
     )
